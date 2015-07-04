7a80d6d28e98df8716d5e0e2faf1c5a295bbe938
pmehrotr <premmemhrotra@hotmail.com>
2015-07-04 09:37:13 -0400

    Addedfixcaps changes

--- SalesTools/src/ca/demo/salestool/Main.java
+++ SalesTools/src/ca/demo/salestool/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println ("hello1 ");
+		System.out.println ("hellocaptial letters fixed ");
 		System.out.println("Add new line1");
+		System.out.println("Captial letters line added...");
+		
 		
 
 	}
