74e010e7731be5d7a3620bd0256b5e559d7bd83f
pmehrotr <premmemhrotra@hotmail.com>
2015-07-04 09:57:48 -0400

    commit after conflicts

--- SalesTools/src/ca/demo/salestool/Main.java
+++ SalesTools/src/ca/demo/salestool/Main.java
@@ -4,10 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println ("hello1 ");
+		System.out.println ("hellocaptial letters fixed ");
 		System.out.println("Add new line1");
-		System.out.println ("Starting display data branch line 1 on Main ");
-		System.out.println ("Added some more displyay data in main ...");
+		System.out.println("Captial letters line added...");
+		
+		
+
 	}
 
 }
