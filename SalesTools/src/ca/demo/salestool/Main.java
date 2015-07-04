cfea984900d7c17724ba1571b4e5f05fca8959db
pmehrotr <premmemhrotra@hotmail.com>
2015-07-03 21:30:22 -0400

    Inital load of files into repository

---
+++ SalesTools/src/ca/demo/salestool/Main.java
@@ -0,0 +1,12 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("hello1 ");
+		
+
+	}
+
+}
---
+++ SalesTools/src/ca/demo/salestool/SalesTools.java
@@ -0,0 +1,10 @@
+package ca.demo.salestool;
+
+public class SalesTools {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
